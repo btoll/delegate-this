5ca3bef17643eaf59ed63c6de03f120af99624bd
Benjamin Toll <benjam72@yahoo.com>
2018-09-29 12:07:04 -0400

    Only register `__INIT__` symbol in symbol registry
    Also, updated readibility of code and comments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console, no-multi-spaces, spaced-comment */
-
 // Other objects relative to { my object }
 //
 //
@@ -22,17 +20,21 @@
 //
 //
 
-const __BASE_PROTO__ = Symbol.for('__BASE_PROTO__');
+// Creating symbols with `.for()` will allow the *same* symbol to be accessed from the
+// symbol registry for the entire module.
+const __BASE_PROTO__ = Symbol();
 const __INIT__       = Symbol.for('__INIT__');
-const __SUPER__      = Symbol.for('__SUPER__');
+const __SUPER__      = Symbol();
 
 const baseProto = {
     [__BASE_PROTO__]: true,
     super: function () {
         try {
-            const caller = this.super.caller;
-            caller[__SUPER__].apply(this, arguments);
-        } catch (e) {
+            this.super.caller[__SUPER__].apply(this, arguments);
+        } // Catch block should never be called b/c we're not attaching a `__SUPER__`
+          // call if the function name isn't found in the prototype chain.
+          // See `enableSuper`.  Leave here for now.
+        catch (e) {
             throw new Error('[ERROR] No super. Sad!');
         }
     }
@@ -41,16 +43,20 @@ const baseProto = {
 const create = (proto, /* optional */ fns) => {
     const p = Object.create(proto);
 
-    // Insert `baseProto` between the first object and the ultimate base (Object.prototype or null).
+    // Insert `baseProto` between the first user object and the ultimate base
+    // (Object.prototype or null).  This is what allows `.super` to be accessed
+    // via the prototype chain.
     if (!proto[__BASE_PROTO__]) {
         Object.setPrototypeOf(
-            getFirstPrototype(proto),
+            getFirstUserPrototype(proto),
             baseProto
         );
     }
 
-    // Since delegate accepts an optional 2nd arg of objects to be mixed in, we have to assign super to them, as well.
+    // Since delegate accepts an optional 2nd arg of an object containing functions
+    // to be mixed into the new object, we have to assign super to them, as well.
     if (fns) {
+        // This is the 'magic' that allows `this.super` to be called.
         enableSuper(fns, proto);
         Object.assign(p, fns);
     }
@@ -63,36 +69,39 @@ const create = (proto, /* optional */ fns) => {
 };
 
 const enableSuper = (obj, proto) => {
-    // Symbols like __INIT__ may want to call super, too.
+    // Also get symbols b/c pre-defined ones like __INIT__ may want to call super, too.
     const keys = Object.getOwnPropertySymbols(obj).concat(Object.keys(obj));
 
     for (let key of keys) {
         const fn = obj[key];
 
-        // TODO: Check to ensure super is a function?
-        if (isFunction(fn) && !fn[__SUPER__]) {
+        // Note that we don't need to check if `proto[key]` actually exists since
+        // `isFunction` will do this.
+        if (
+            !fn[__SUPER__] &&
+            isFunction(proto[key]) &&
+            isFunction(fn)
+        ) {
             fn[__SUPER__] = proto[key];
         }
     }
 };
 
-// Walk the prototype chain to get the first prototype in 'userland' (the 'base proto' in the chain above).
-const getFirstPrototype = proto => {
-    if (isFirstPrototype(proto)) {
+// Walk the prototype chain to get the first prototype in user space (the 'base proto'
+// in the chain above).
+const getFirstUserPrototype = proto => {
+    if (isBasePrototype(proto)) {
         return proto;
     }
 
-    return getFirstPrototype(Object.getPrototypeOf(proto));
+    return getFirstUserPrototype(Object.getPrototypeOf(proto));
 };
 
-const isFirstPrototype = proto =>
+const isBasePrototype = proto =>
     Object.getPrototypeOf(proto) === (Object.prototype || null);
 
 const isFunction = fn =>
     Object.prototype.toString.call(fn) === '[object Function]';
 
-module.exports = {
-    __INIT__,
-    create
-};
+module.exports = create;
 
